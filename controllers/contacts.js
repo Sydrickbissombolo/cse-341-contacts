a1d42c31de5239159602e6d2878c284bdc1f0ee7
Sydrickbissombolo <newtonbissomblo2@gmail.com>
2025-03-08 23:07:34 +0100

    Fixed error preventing retrival by ID

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,12 +10,21 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
-    result.toArray().then((contacts) => {
+    try {
+        const contactId = new ObjectId(req.params.id);
+
+        // Use findOne instead of find()
+        const contacts = await mongodb.getDatabase().db().collection('contacts').findOne({ _id: contactId });
+
+        if (!contacts) {
+            return res.status(404).json({ message: "Contact not found." });
+        }
+
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts(0));
-    });
+        res.status(200).json(contacts);
+    } catch (error) {
+        res.status(500).json({ message: "Invalid ID format or server error.", error: error.message });
+    }
 };
 
 module.exports = {
